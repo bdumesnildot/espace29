6f666ab37680ba99c3fd686ad5581d7aa4f5ec03
bdumesnildot <benoit.du-mesnildot@jdc.fr>
2025-10-16 23:51:03 +0200

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,25 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from "astro/config";
+import tailwindcss from "@tailwindcss/vite"
+
+const { APP_BASE_URL } = process.env
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: APP_BASE_URL,
+  compressHTML: true,
+  build: {
+    inlineStylesheets: "auto",
+  },
+  vite: {
+    build: {
+      assetsInlineLimit: 0,
+    },
+    plugins: [tailwindcss()],
+  },
+  env: {
+    schema: {
+      APP_BASE_URL: envField.string({ context: "server", access: "public"}),
+    }
+  }
+})
