cc8db96c3055bb9852e49ba51707f3901300f363
bdumesnildot <benoit.du-mesnildot@jdc.fr>
2025-10-17 15:24:12 +0200

    mobile navbar

---
+++ src/lib/navigation-helper.ts
@@ -0,0 +1,7 @@
+export const navigationItems = [
+  { href: "/", label: "Espace29" },
+  { href: "/infos", label: "Infos" },
+  { href: "/agenda", label: "Agenda" },
+  { href: "/artistes", label: "Artistes" },
+  { href: "/dev/design-system", label: "Design system" },
+]
