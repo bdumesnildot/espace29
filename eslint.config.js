a7215e09f2c359c14fb87067bca0b14c35c6b32b
bdumesnildot <benoit.du-mesnildot@jdc.fr>
2025-10-17 08:27:17 +0200

    tooling eslint prettier

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import eslintPluginAstro from "eslint-plugin-astro"
+
+export default [
+  // Global ignores
+  {
+    ignores: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/build/**",
+      "**/coverage/**",
+      "**/generated/**",
+      "**/public/**",
+      "**/out/**",
+      "**/cache/**",
+      "**/eslint.config.js",
+    ],
+  },
+
+  // add more generic rule sets here, such as:
+  // js.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  {
+    rules: {
+      // override/add rules settings here, such as:
+      // "astro/no-set-html-directive": "error"
+    },
+  },
+]
