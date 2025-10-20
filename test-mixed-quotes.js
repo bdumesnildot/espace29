7700f85a8310398d83c027b833f1c97eafaf2b5e
bdumesnildot <benoit.du-mesnildot@jdc.fr>
2025-10-20 16:54:49 +0200

    test: verify mixed quote formatting in pre-commit

---
+++ test-mixed-quotes.js
@@ -0,0 +1,8 @@
+import test from "test-module"
+import { something } from "another-module"
+import "./styles.css"
+export { default } from "some-export"
+
+const message = 'Hello world'
+const anotherString = 'This should stay single quoted'
+console.log(message)
