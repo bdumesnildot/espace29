fd4a2dea0a0ab503e8bae500e4f1e4e992c7d07d
bdumesnildot <benoit.du-mesnildot@jdc.fr>
2025-10-21 21:34:44 +0200

    first global fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,7 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {
-      fontSize: {
-        // Legacy-specific typography scale with embedded weights
-        "8xl": ["120px", { lineHeight: "1", fontWeight: "600" }],
-        "7xl": ["100px", { lineHeight: "1", fontWeight: "600" }],
-        "6xl": ["80px", { lineHeight: "1", fontWeight: "400" }],
-        "5xl": ["60px", { lineHeight: "1", fontWeight: "400" }],
-        "4xl": ["35px", { lineHeight: "1.2", fontWeight: "400" }],
-        "3xl": ["30px", { fontWeight: "400" }],
-        "2xl": ["25px", { fontWeight: "400" }],
-        xl: ["20px", { fontWeight: "400" }],
-        lg: ["15px", { lineHeight: "1.6", fontWeight: "400" }],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
