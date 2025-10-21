554822de13264439be67c642526b44a8bdce7b2d
bdumesnildot <benoit.du-mesnildot@jdc.fr>
2025-10-21 16:28:20 +0200

    feat: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,44 @@
 import { defineConfig, envField } from "astro/config"
+import dotenv from "dotenv"
 import tailwindcss from "@tailwindcss/vite"
+import vercel from "@astrojs/vercel/serverless"
 
+dotenv.config()
 const { APP_BASE_URL } = process.env
 
 // https://astro.build/config
 export default defineConfig({
   site: APP_BASE_URL,
   compressHTML: true,
+
   build: {
     inlineStylesheets: "auto",
   },
+
   vite: {
     build: {
       assetsInlineLimit: 0,
     },
     plugins: [tailwindcss()],
   },
+
   env: {
     schema: {
       APP_BASE_URL: envField.string({ context: "server", access: "public" }),
     },
   },
+
+  image: {
+    domains: ["picsum.photos"],
+    remotePatterns: [{ protocol: "https" }],
+  },
+
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: false,
+    },
+    maxDuration: 8,
+    im,
+  }),
 })
